73ff1a9f50a718b6f9a85ff13837206762ba64df
FranciscoFloat <perezmerinojosefrancisco@gmail.com>
2025-09-24 21:15:22 -0600

    fix

--- src/components/Libros/BookViewer.tsx
+++ src/components/Libros/BookViewer.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 interface BookViewerProps {
   fileName: string;
   title: string;
--- src/components/Libros/Libros.tsx
+++ src/components/Libros/Libros.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import BookCard from './BookCard';
 import BookViewer from './BookViewer';
 
